a1f77f7ff37e14b4ad37291a1ab55d5752798cf7
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 14:46:33 -0500

    Add project files.

---
+++ BirthdayScenarioGame/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BirthdayScenarioGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateButton = new System.Windows.Forms.Button();
+            this.dayTextBox = new System.Windows.Forms.TextBox();
+            this.monthTextBox = new System.Windows.Forms.TextBox();
+            this.scenarioLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(112, 155);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // dayTextBox
+            // 
+            this.dayTextBox.Location = new System.Drawing.Point(161, 81);
+            this.dayTextBox.Name = "dayTextBox";
+            this.dayTextBox.Size = new System.Drawing.Size(25, 20);
+            this.dayTextBox.TabIndex = 1;
+            this.dayTextBox.Text = "DD";
+            // 
+            // monthTextBox
+            // 
+            this.monthTextBox.Location = new System.Drawing.Point(112, 81);
+            this.monthTextBox.Name = "monthTextBox";
+            this.monthTextBox.Size = new System.Drawing.Size(25, 20);
+            this.monthTextBox.TabIndex = 2;
+            this.monthTextBox.Text = "MM";
+            // 
+            // scenarioLabel
+            // 
+            this.scenarioLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scenarioLabel.Location = new System.Drawing.Point(28, 119);
+            this.scenarioLabel.Name = "scenarioLabel";
+            this.scenarioLabel.Size = new System.Drawing.Size(250, 23);
+            this.scenarioLabel.TabIndex = 3;
+            this.scenarioLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Birthday Scenario Game";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "by Joseph \"Alex\" Puckett";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter your birthday below";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "and click \"Generate\" for a scenario!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 191);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scenarioLabel);
+            this.Controls.Add(this.monthTextBox);
+            this.Controls.Add(this.dayTextBox);
+            this.Controls.Add(this.generateButton);
+            this.Name = "Form1";
+            this.Text = "Birthday Scenario Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox dayTextBox;
+        private System.Windows.Forms.TextBox monthTextBox;
+        private System.Windows.Forms.Label scenarioLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BirthdayScenarioGame/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BirthdayScenarioGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int MONTH_SCENARIOS_SIZE = 12; //Size of monthScenarios array.
+            const int DAY_SCENARIOS_SIZE = 31; //Size of dayScenarios array.
+
+            int month; //User-defined month.
+            int day; //User-defined day.
+            string scenario; //Computer-generated scenario.
+
+            int index = 0; //Index to go through slots in array.
+
+            //declare arrays.
+            string[] monthScenarios = new string[MONTH_SCENARIOS_SIZE];
+            string[] dayScenarios = new string[DAY_SCENARIOS_SIZE];
+
+            StreamReader inputFile; //Declare variable to read files.
+
+            //Read the MonthScenarios.txt file.
+            inputFile = File.OpenText("MonthScenarios.txt"); //Open MonthScenarios.txt.
+            while (index < monthScenarios.Length && !inputFile.EndOfStream) //While the index is less than monthScenarios.Length and the input file isn't at the end...
+            {
+                monthScenarios[index] = inputFile.ReadLine(); //Read the current line and set it equal to the current index in monthScenarios.
+                index++; //Advance the index counter by one.
+            }
+            inputFile.Close(); //Close the file.
+
+            index = 0; //Reset index counter to zero.
+
+            //Read the DayScenarios.txt file.
+            inputFile = File.OpenText("DayScenarios.txt"); //Open DayScenarios.txt.
+            while (index < dayScenarios.Length && !inputFile.EndOfStream) //While the index is less than dayScenarios.Length and the input file isn't at the end...
+            {
+                dayScenarios[index] = inputFile.ReadLine(); //Read the current line and set it equal to the current index in dayScenarios.
+                index++; //Advance the index counter by one.
+            }
+            inputFile.Close(); //Close the file.
+
+            month = int.Parse(monthTextBox.Text) - 1; //Retrieve value from the month text box.
+            day = int.Parse(dayTextBox.Text) - 1; //Retreive value from the day text box.
+
+            scenario = monthScenarios[month] + dayScenarios[day]; //Create the scenario.
+            scenarioLabel.Text = scenario; //Display the scenario.
+        }
+    }
+}
